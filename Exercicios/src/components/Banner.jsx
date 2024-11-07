7adb58604ebb1316a4b7f4901be461f5c1244413
Rodrigo Graça <rodrigomgraca@ua.pt>
2024-11-07 19:01:52 +0000

    banner

---
+++ Exercicios/src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Banner() {
+    return (
+        <div className="flex flex-col items-center h-80 w-full p-6 mb-6 rounded-3xl"
+            style={{
+                background: 'rgba(0, 0, 0, 0.5)',
+                color: 'white',
+            }}>
+
+        </div>
+    );
+}
+
+export default Banner;
--- Exercicios/src/pages/Index.jsx
+++ Exercicios/src/pages/Index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardExercicio from '../components/CardExercicio';
 import Navbar from '../components/Navbar';
+import Banner from '../components/Banner';
 
 function IndexPage() {
     return (
@@ -14,14 +15,28 @@ function IndexPage() {
                     backgroundPosition: 'center',
                 }}
             >
-                <h1 className="text-4xl font-bold text-white py-12">Índice de Exercícios</h1>
-                <div className="flex flex-wrap justify-center gap-12 mt-6">
-                    <CardExercicio to="/todolist" title="Exercício 1 - ToDo List" />
-                    <CardExercicio to="/todolist-proptypes" title="Exercício 2 - Better ToDo" />
-                    <CardExercicio to="/notfound" title="Not Found" />
+                <div className="flex flex-col items-center rounded-3xl p-6"
+                    style={{
+                        background: 'rgba(0, 0, 0, 0.2)',
+                        backdropFilter: 'blur(2px)',
+                        color: 'white',
+                    }}>
+                    <Banner />
+                    <div className="flex flex-col items-center p-6 rounded-3xl"
+                        style={{
+                            background: 'rgba(255, 255, 255, 0.05)',
+                            color: 'white',
+                        }}>
+                        <h1 className="text-4xl font-bold text-white pb-6">Índice de Exercícios</h1>
+                        <div className="flex flex-wrap justify-center gap-12 mt-6">
+                            <CardExercicio to="/todolist" title="Exercício 1 - ToDo List" />
+                            <CardExercicio to="/todolist-proptypes" title="Exercício 2 - Better ToDo" />
+                            <CardExercicio to="/notfound" title="Not Found" />
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
 
